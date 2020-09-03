a51866f5d9d8871c04b7c7a51c8c272fefe45379
Ivan <okoroque@gmail.com>
2020-09-03 16:57:37 -0500

    prep to production

--- js/main.js
+++ js/main.js
@@ -103,14 +103,3 @@ function watchScroll(){
 function scrollToTop(){
   window.scrollTo(0, 0);
 }
-
-//fix links temporary
-window.onload = function(){
-  ar = document.getElementsByTagName("a")
-  for(let i = 0; i< ar.length; i++){
-    if(ar[i].pathname.substring(0,9) == "/classes/"){
-      ar[i].href = ar[i].href.replace("/classes/", "/wilkelabtest/classes/")
-      console.log(ar[i])
-    } 
-  }
-}
